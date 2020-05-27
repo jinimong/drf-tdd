beedcf04d9ba585afeac9d36a22dd879ac29efff
jwkim <phoongdeng@gmail.com>
2020-05-27 15:54:06 +0900

    Add Fail Test about User Model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_sucessful(self):
+        """ Test creating a new user with an email is successful """
+        email = "test@jinimong.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
