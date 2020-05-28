ebdb9659d2219140a6f7767e16bee56771b2b455
jwkim <phoongdeng@gmail.com>
2020-05-28 18:03:50 +0900

    Add tests for creating a new token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 def create_user(**params):
@@ -58,3 +59,48 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
+
+
+def test_create_token_for_user(self):
+    """ Test that a token is created for the user """
+    payload = {
+        "email": "test@jinimong.com",
+        "password": "testpass",
+    }
+    create_user(**payload)
+    res = self.client.post(TOKEN_URL, payload)
+
+    self.assertEqual(res.status_code, status.HTTP_200_OK)
+    self.assertIn("token", res.data)
+
+
+def test_create_token_invalid_credentials(self):
+    """ Test that token is not created if invalid credentials are given """
+    payload = {
+        "email": "test@jinimong.com",
+        "password": "testpass",
+    }
+    create_user(**payload)
+    res = self.client.post(TOKEN_URL, {"email": payload["email"], "password": "wrong"})
+
+    self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertNotIn("token", res.data)
+
+
+def test_create_token_no_user(self):
+    """ Test that token is not created if user doesn't exist """
+    payload = {
+        "email": "test@jinimong.com",
+        "password": "testpass",
+    }
+    res = self.client.post(TOKEN_URL, payload)
+
+    self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertNotIn("token", res.data)
+
+
+def test_create_token_missing_field(self):
+    """ Test that email and password are required """
+    res = self.client.post(TOKEN_URL, {"email": "one", "password": ""})
+    self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertNotIn("token", res.data)
